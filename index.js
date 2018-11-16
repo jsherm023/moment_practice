672fcd5af3b22ebf7c77198a16b59719254772d3
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-15 19:26:51 -0600

    Add math, gem, install webpack.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var moment = require('moment');
+var math = require('mathjs');
+
 console.log("i'm in index.js");
+console.log(math.sqrt(-4));
 console.log(moment("20241031", "YYYYMMDD").fromNow());
