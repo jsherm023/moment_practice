fadc57003c215e663a82cd337ba0b92b3f41a6af
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-15 19:59:20 -0600

    Add babel.

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var math = require('mathjs');
 console.log("i'm in index.js");
 console.log(math.sqrt(-4));
 console.log(moment("20241031", "YYYYMMDD").fromNow());
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`)
+console.log("goodbye");
